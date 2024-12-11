3a098683c5dbfd3eea9d9776cd742a7f875c631e
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-11 14:15:45 +0300

    change additemForm

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,47 +1,69 @@
-import {Button} from "./Button";
 import {ChangeEvent, KeyboardEvent, useState} from "react";
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField';
 
 
 type PropsType = {
-	addItem: (title:string) => void
+    addItem: (title: string) => void
 }
 
 export const AddItemForm = ({addItem}: PropsType) => {
 
-	const [title, setTitle] = useState('')
-	const [error, setError] = useState<string | null>(null)
+    const [title, setTitle] = useState('')
+    const [error, setError] = useState<string | null>(null)
 
-	const addItemHandler = () => {
-		if (title.trim() !== '') {
-			addItem(title.trim())
-			setTitle('')
-		} else {
-			setError('Title is required')
-		}
+    const addItemHandler = () => {
+        if (title.trim() !== '') {
+            addItem(title.trim())
+            setTitle('')
+        } else {
+            setError('Title is required')
+        }
+    }
+
+    const changeItemHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const addItemOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        setError(null)
+        if (event.key === 'Enter') {
+            addItemHandler()
+        }
+    }
+
+	const  styles = {
+		backgroundColor: 'darkgreen',
+		maxWidth: '38px',
+		maxHeight: '38px',
+		minWidth: '38px',
+		minHeight: '38px'
 	}
 
-	const changeItemHandler = (event: ChangeEvent<HTMLInputElement>) => {
-		setTitle(event.currentTarget.value)
-	}
+    return (
+        <div>
+            {/*<input*/}
+            {/*    className={error ? 'error' : ''}*/}
+            {/*    value={title}*/}
+            {/*    onChange={changeItemHandler}*/}
+            {/*    onKeyUp={addItemOnKeyUpHandler}*/}
+            {/*/>*/}
+            <TextField
+                error={!!error}
+                // helperText={error} Можно такой вариант!!!!
+                size='small'
+                id="outlined-basic"
+                label={error ? 'Title is required' : 'Type smth...'}
+                variant="outlined"
+                value={title}
+                onChange={changeItemHandler}
+                onKeyUp={addItemOnKeyUpHandler}
+            />
 
-	const addItemOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-		setError(null)
-		if (event.key === 'Enter') {
-			addItemHandler()
-		}
-	}
-	return (
-		<div>
-			<input
-				className={error ? 'error' : ''}
-				value={title}
-				onChange={changeItemHandler}
-				onKeyUp={addItemOnKeyUpHandler}
-			/>
-			<Button title={'+'} onClick={addItemHandler}/>
-			{error && <div className={'error-message'}>{error}</div>}
-		</div>
-	)
+            <Button variant="contained" size="small" onClick={addItemHandler} style={styles}>+</Button>
+            {/*{error && <div className={'error-message'}>{error}</div>}*/}
+        </div>
+    )
 }
 
 
